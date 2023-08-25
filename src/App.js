1c8626758ad7559bd44fa64bb431d49ec0320831
Firdosh01 <firdosh7392@gmail.com>
2023-08-25 09:58:46 -0500

    redux implement started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
+
+
+
   return (
     <>
-    Let's start
+    <Todos />
     </>
     );
 }
